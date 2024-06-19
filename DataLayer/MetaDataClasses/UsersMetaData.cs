63bf6e5771cad18cdcaec367e55f8ceefec736e4
Majid <majid.asadollahi95@gmail.com>
2024-06-19 21:47:25 +0330

    init commit

---
+++ DataLayer/MetaDataClasses/UsersMetaData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class UsersMetaData
+    {
+        public int UserID { get; set; }
+        public int RoleID { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ActiveCode { get; set; }
+        public bool IsActive { get; set; }
+        public System.DateTime RegisterDate { get; set; }
+
+        public virtual Roles Roles { get; set; }
+    }
+}
---
+++ MyEshop/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyEshop.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
