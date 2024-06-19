51f9665560f906427e26d9789e6a3b85720ceee8
Majid <majid.asadollahi95@gmail.com>
2024-06-20 00:45:35 +0330

    add register

--- MyEshop/Controllers/AccountController.cs
+++ MyEshop/Controllers/AccountController.cs
@@ -3,16 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DataLayer;
+using DataLayer.ViewModels;
+using System.Web.Security;
+
 
 namespace MyEshop.Controllers
 {
     public class AccountController : Controller
     {
+        private MyEshop_DBEntities db = new MyEshop_DBEntities();
         // GET: Account
         public ActionResult Register()
         {
             return View();
         }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterViewModel register)
+        {
+            if (ModelState.IsValid)
+            {
+                if (!db.Users.Any(u=>u.Email == register.Email.Trim().ToLower()))
+                {
+                    Users user = new Users()
+                    {
+                        Email = register.Email.Trim().ToLower(),
+                        Password = FormsAuthentication.HashPasswordForStoringInConfigFile(register.Password, "SHA256"),
+                        ActiveCode = Guid.NewGuid().ToString(),
+                        IsActive = false,
+                        RegisterDate = DateTime.Now,
+                        RoleID = 1,
+                        UserName = register.UserName,
+                    };
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                    return View("SuccessRegister", user);
+                }
+                else
+                {
+                    ModelState.AddModelError("Email", "ایمیل وارد شده تکراری است");
+                }
+            }
+
+            return View(register);
+        }
+
 
 
         public ActionResult Login()
