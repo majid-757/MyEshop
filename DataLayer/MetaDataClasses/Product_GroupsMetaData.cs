b2a1f98a612df5784bdb1c8e2c63410c80e918d3
Majid <majid.asadollahi95@gmail.com>
2024-06-24 22:34:08 +0330

    develop admin panel

---
+++ DataLayer/MetaDataClasses/Product_GroupsMetaData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class Product_GroupsMetaData
+    {
+        [Key]
+        public int GroupID { get; set; }
+
+
+        [Display(Name = "عنوان گروه")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string GroupTitle { get; set; }
+        public Nullable<int> ParentID { get; set; }
+    }
+}
---
+++ MyEshop/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataLayer;
+
+namespace MyEshop.Controllers
+{
+    public class ProductController : Controller
+    {
+        MyEshop_DBEntities db = new MyEshop_DBEntities();
+        public ActionResult ShowGroups()
+        {
+            return PartialView(db.Product_Groups.ToList());
+        }
+    }
+}
