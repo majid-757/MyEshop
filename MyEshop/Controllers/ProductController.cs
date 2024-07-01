cc011f07cf91914678b42979c177a617ee901a90
Majid <majid.asadollahi95@gmail.com>
2024-07-01 23:32:45 +0330

    finish filter and pagination

--- MyEshop/Controllers/ProductController.cs
+++ MyEshop/Controllers/ProductController.cs
@@ -20,7 +20,7 @@ namespace MyEshop.Controllers
 
         public ActionResult LastProduct()
         {
-            return PartialView(db.Products.OrderByDescending(p=>p.CreateDate).Take(12));
+            return PartialView(db.Products.OrderByDescending(p => p.CreateDate).Take(12));
         }
 
 
@@ -29,10 +29,10 @@ namespace MyEshop.Controllers
         {
             var product = db.Products.Find(id);
             ViewBag.ProductFeatures =
-                product.Product_Features.DistinctBy(f=>f.FeaturesID).Select(f=>new ShowProductFeatureViewModel()
+                product.Product_Features.DistinctBy(f => f.FeaturesID).Select(f => new ShowProductFeatureViewModel()
                 {
                     FeatureTitle = f.Features.FeaturesTitle,
-                    Values = db.Product_Features.Where(fe=>fe.FeaturesID == f.FeaturesID).Select(fe=>fe.Value).ToList()
+                    Values = db.Product_Features.Where(fe => fe.FeaturesID == f.FeaturesID).Select(fe => fe.Value).ToList()
                 }).ToList();
             if (product == null)
             {
@@ -45,7 +45,7 @@ namespace MyEshop.Controllers
 
         public ActionResult ShowComments(int id)
         {
-            return PartialView(db.Product_Comments.Where(c=>c.ProductID == id));
+            return PartialView(db.Product_Comments.Where(c => c.ProductID == id));
         }
 
 
@@ -69,10 +69,59 @@ namespace MyEshop.Controllers
                 db.Product_Comments.Add(productComments);
                 db.SaveChanges();
 
-                return PartialView("ShowComments", db.Product_Comments.Where(c=>c.ProductID == productComments.ProductID));
+                return PartialView("ShowComments", db.Product_Comments.Where(c => c.ProductID == productComments.ProductID));
             }
 
             return PartialView(productComments);
+        }
+
+
+
+        [Route("Archive")]
+        public ActionResult ArchiveProduct(int pageId = 1, string title = "", int minPrice = 0, int maxPrice = 0, List<int> selectedGroups = null)
+        {
+            ViewBag.Groups = db.Product_Groups.ToList();
+            ViewBag.productTitle = title;
+            ViewBag.minPrice = minPrice;
+            ViewBag.maxPrice = maxPrice;
+            ViewBag.pageId = pageId;
+            ViewBag.selectGroup = selectedGroups;
+
+            List<Products> list = new List<Products>();
+
+            if (selectedGroups != null && selectedGroups.Any())
+            {
+                foreach (int group in selectedGroups)
+                {
+                    list.AddRange(db.Product_Selected_Groups.Where(g => g.GroupID == group).Select(g => g.Products).ToList());
+                }
+
+                list = list.Distinct().ToList();
+            }
+            else
+            {
+                list.AddRange(db.Products.ToList());
+            }
+
+            if (title != "")
+            {
+                list = list.Where(p => p.ProductTitle.Contains(title)).ToList();
+            }
+
+            if (minPrice > 0)
+            {
+                list = list.Where(p => p.Price >= minPrice).ToList();
+            }
+            if (maxPrice > 0)
+            {
+                list = list.Where(p => p.Price <= maxPrice).ToList();
+            }
+
+            //paging
+            int take = 9;
+            int skip = (pageId - 1) * take;
+            ViewBag.PageCount = list.Count() / take;
+            return View(list.OrderByDescending(p => p.CreateDate).Skip(skip).Take(take).ToList());
         }
     }
 }
---
+++ MyEshop/Controllers/SearchController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataLayer;
+
+namespace MyEshop.Controllers
+{
+    public class SearchController : Controller
+    {
+        private MyEshop_DBEntities db = new MyEshop_DBEntities();
+
+        public ActionResult Index(string q)
+        {
+            List<Products> list = new List<Products>();
+
+            list.AddRange(db.Product_Tags.Where(t=>t.Tag == q).Select(t=>t.Products).ToList());
+            
+            list.AddRange(db.Products.Where(p=>p.ProductTitle.Contains(q) || p.ShortDescription.Contains(q) || p.Text.Contains(q)).ToList());
+
+            ViewBag.Search = q;
+            return View(list.Distinct());
+        }
+    }
+}
