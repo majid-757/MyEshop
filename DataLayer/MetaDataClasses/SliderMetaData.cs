ccebd8ea9d19c71742e70907dddc90094b407cc0
Majid <majid.asadollahi95@gmail.com>
2024-07-05 02:54:36 +0330

    add slider

---
+++ DataLayer/MetaDataClasses/SliderMetaData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class SliderMetaData
+    {
+
+        [Key]
+        public int SlideID { get; set; }
+
+
+        [Display(Name = "عنوان")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string Title { get; set; }
+
+
+        [Display(Name = "تصویر")]
+        public string ImageName { get; set; }
+
+
+        [Display(Name = "تاریخ شروع")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        public System.DateTime StartDate { get; set; }
+
+
+        [Display(Name = "تاریخ پایان")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        public System.DateTime EndDate { get; set; }
+
+
+        [Display(Name = "وضعیت")]
+        public bool IsActive { get; set; }
+
+
+
+        [Display(Name = "آدرس")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [Url(ErrorMessage = "آدرس وارد شده معتبر نمی باشد")]
+        public string Url { get; set; }
+    }
+}
--- MyEshop/Controllers/HomeController.cs
+++ MyEshop/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DataLayer;
 
 namespace MyEshop.Controllers
 {
     public class HomeController : Controller
     {
+        private MyEshop_DBEntities db = new MyEshop_DBEntities();
+
         // GET: Home
         public ActionResult Index()
         {
@@ -23,7 +26,8 @@ namespace MyEshop.Controllers
 
         public ActionResult Slider()
         {
-            return PartialView();
+            DateTime dt = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 0, 0, 0);
+            return PartialView(db.Slider.Where(s => s.IsActive && s.StartDate <= dt && s.EndDate >= dt));
         }
     }
 }
